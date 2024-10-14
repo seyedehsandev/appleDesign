499072ced87b5ecdd0c328c8d1e90aecef981758
Ehsan <dev.ehsanhosseini@gmail.com>
2024-10-14 21:51:46 +0330

    desiging main section layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,17 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-       navbg : "rgba(22, 22, 23, .8)"
+       "navbg" : "rgba(22, 22, 23, .8)" , 
+       "btncolor" : "#0071e3"
+       
       } , 
       screens: {
 
         'md': '834px',
+      } ,
+      backgroundImage: {
+        'hero-pattern': "url('./images/iphone16.jpg')",
+        'footer-texture': "url('/img/footer-texture.png')",
       }
     },
   },
