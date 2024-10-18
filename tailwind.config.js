db36fa2fbbad2baaed5a19b5c11dd05d0aa70ee7
Ehsan <dev.ehsanhosseini@gmail.com>
2024-10-18 21:19:15 +0330

    finishing mini cards

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,20 @@ module.exports = {
     extend: {
       colors: {
        "navbg" : "rgba(22, 22, 23, .8)" , 
-       "btncolor" : "#0071e3"
+       "btncolor" : "#0071e3",
+       "footerbg" : "rgb(245, 245, 247)",
+       "footertext" : "rgba(0, 0, 0, 0.56)",
+
       } , 
       screens: {
 
+        "sm": "735px",
         'md': '834px',
+        "lg": "1069px",
+        
       } ,
       backgroundImage: {
+
         'hero-pattern': "url('./images/iphone16.jpg')",
         'hero-mini': "url('./images/16promini.jpg')",
         'iphone': "url('/images/ooo.jpg')",
@@ -19,6 +26,12 @@ module.exports = {
         'watch': "url('/images/watch.jpg')",
         'airpod': "url('/images/airpodmini.jpg')",
         'watchmobile': "url('/images/watchmobile.jpg')",
+        'ultra': "url('/images/ulrta1.jpg')",
+        'trade': "url('/images/tradein.jpg')",
+        'ipadmini': "url('/images/ipadmini.jpg')",
+        'cardbg': "url('/images/cardbg.jpg')",
+
+
 
 
 
